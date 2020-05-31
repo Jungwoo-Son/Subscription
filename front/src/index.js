00d9ecfc9313d5a1caef4cac8585e27bea53068d
Jungwoo-Son <jungwoo9356@gmail.com>
2020-05-31 15:09:22 +0900

    feat: Improve api of the styleReciever
    styleReciever의 api를 개선시켰다.

--- front/src/index.js
+++ front/src/index.js
@@ -11,20 +11,21 @@ import './index.css'
 
 
 
-ReactDOM.render(
-    <React.StrictMode>
-        <DisplayedVideo src="https://i.ytimg.com/vi/qZ1SNmXx7F8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDGUDxzqEwMp9iKIWJKAyXo7NWhdA"/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
 
 // ReactDOM.render(
-//   <React.StrictMode>
-//     <NavBar id='navigationbar'/>
-//     <TitleBar id='titlebar'/>
-//     <Contents id='contents'/>
-//   </React.StrictMode>,
-//   document.getElementById('root')
+//     <React.StrictMode>
+//         <DisplayedVideo src="https://i.ytimg.com/vi/qZ1SNmXx7F8/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDGUDxzqEwMp9iKIWJKAyXo7NWhdA"/>
+//     </React.StrictMode>,
+//     document.getElementById('root')
 // );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NavBar id='navigationbar'/>
+    <TitleBar id='titlebar'/>
+    <Contents id='contents'/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
--- front/src/utils/styleReciever.js
+++ front/src/utils/styleReciever.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './styleReciever.css';
 
-function giveStyleInterecter_(children) {
-    const class_name = this.props.className;
-    console.log(class_name);
-    return (
-        <div className={'style-reciever '+(class_name?class_name:'')} id={this.props.id}>
-            {children}
-        </div>
-    );
+function giveStyleInterecter_(props, element) {
+    const class_name = props.className;
+    
+    let props_copied = Object.assign({},props);
+    
+    props_copied.className = 'style-reciever ' + (element.props.className?element.props.className:' ') + (class_name?class_name:' ');
+    return React.cloneElement(element, props_copied, element.props.children);
 }
-const giveStyleReciever = (that, children)=>giveStyleInterecter_.bind(that, children)();
+// const giveStyleReciever = (that, element)=>giveStyleInterecter_.bind(that, element)();
+const giveStyleReciever = giveStyleInterecter_;
 export default giveStyleReciever;
