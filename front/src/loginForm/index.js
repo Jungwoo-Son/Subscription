80d504209411206a7d3c42bae0e86517d24951eb
Jungwoo-Son <jungwoo9356@gmail.com>
2020-06-07 16:03:32 +0900

    feat: Add action to login form

--- front/src/loginForm/index.js
+++ front/src/loginForm/index.js
@@ -7,9 +7,9 @@ function LoginForm (props) {
 
 
     return (
-        <form id='login-form'>
+        <form id='login-form' method='get' action='http://localhost:8080/auth'>
             <div id='login-form-title'>Subscriber</div>
-            <button id='login-button'>구글 로그인으로 시작하기</button>
+            <input type='submit' id='login-button' value='구글 로그인으로 시작하기'/>
         </form>
     );
 
