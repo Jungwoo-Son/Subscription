6616fb0173a27e8d760ab42ad7a0af10a377d4b6
Jungwoo-Son <jungwoo9356@gmail.com>
2020-06-07 16:52:25 +0900

    feat: Add login system

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { useCookies } from 'react-cookie';
 
 import LoginForm from './loginForm';
 import TitleBar from './titlebar';
@@ -13,21 +14,38 @@ import './index.css'
 
 
 
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <LoginForm />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
-
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar id='navigationbar'/>
-    <TitleBar id='titlebar'/>
-    <Contents id='contents'/>
+    <DispatchFirstPage/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function DispatchFirstPage() {
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
+  
+  if (cookies.token === undefined) {
+    return <LoginPage/>;
+  }
+  else {
+    return <MainPage/>;
+  }
+}
+
+
+function LoginPage() {
+  return <LoginForm />;
+}
+
+function MainPage() {
+  return (
+    <>
+      <NavBar id='navigationbar'/>
+      <TitleBar id='titlebar'/>
+      <Contents id='contents'/>
+    </>
+  );
+}
+
 
 serviceWorker.unregister();
